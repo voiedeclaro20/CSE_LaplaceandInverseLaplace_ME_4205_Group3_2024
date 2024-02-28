77ae95b59625bf47b4fbbc9663d19bdae65fdb8b
Voy <161600364+voiedeclaro20@users.noreply.github.com>
2024-02-28 21:34:04 +0800

    Update Laboratory1_Part2.m

--- Laboratory1_Part2.m
+++ Laboratory1_Part2.m
@@ -7,14 +7,14 @@ syms t s
 
 %% PART 2 - Inverse Laplace Transform 
 
-f1 = (8-3*s+s^2)/(s^3);
-F1 = ilaplace(f1);
-pretty(F1)
+f4 = (8-3*s+s^2)/(s^3);
+F4 = ilaplace(f4);
+pretty(F4)
 
-f2 = ((5)/(s-2))-((4*s)/(s^2+9));
-F2 = ilaplace(f2);
-pretty(F2)
+f5 = ((5)/(s-2))-((4*s)/(s^2+9));
+F5 = ilaplace(f5);
+pretty(F5)
 
-f3 = 7/(s^2+6);
-F3 = ilaplace(f3);
-pretty(F3)
+f6 = 7/(s^2+6);
+F6 = ilaplace(f6);
+pretty(F6)
